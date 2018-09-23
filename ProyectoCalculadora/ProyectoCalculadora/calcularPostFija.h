8b7f7bc793dd1b64a828d49c102c52b85279347b
JonathanVM <jonathanvasquezmora@gmail.com>
2018-09-22 19:03:44 -0600

    cambios

--- ProyectoCalculadora/ProyectoCalculadora/calcularPostFija.h
+++ ProyectoCalculadora/ProyectoCalculadora/calcularPostFija.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef CALCULARPOSTFIJA_H
+#define CALCULARPOSTFIJA_H
+
+#include <iostream>
+#include <string>
+#include "pila.h"
+
+class CalcularPostFija {
+public:
+	CalcularPostFija();
+
+
+private: 
+	Pila numeros;
+	Lista exprecion;
+};
+
+#endif // !CALCULARPOSTFIJA_H
